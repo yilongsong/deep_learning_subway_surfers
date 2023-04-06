96790ecf642e4626af091f1c18f95cc07f50a9fe
Yilong Song <songyilong18@gmail.com>
2023-04-06 12:18:24 +0200

    changed title

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+# main.py
+# For running trained models
+# Author: yilong song
+# Date: Apr 6, 2023
+
+
+def play():
+    while True: # Infinite loop
+        
+def open_game()
+
+def main():
+    # Open game
+
+    # Start playing
+
+    
